b2a43ae8c1c26e34bb2a7f394e97c05c4189f996
Anurag <anuragmarketing101@gmail.com>
2022-11-06 16:17:48 +0530

    homePageSidebar-tips-added

--- frontend-nextjs/components/Sidebar/HomePageSidebar/HomePageSidebar.tsx
+++ frontend-nextjs/components/Sidebar/HomePageSidebar/HomePageSidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {GoPrimitiveDot} from "react-icons/go"
 import { Blog } from '../../../types/typings'
+import ReadingListTip from '../Tips/ReadingListTip'
 import StaffPicks from './Staff Picks/StaffPicks'
 
 import StaffPicksBlogCard from './StaffPicksBlogCard'
@@ -12,10 +13,11 @@ interface Props {
 
 const HomePageSidebar = ({blogs}:Props) => {
   return (
-    <div className='w-[90%] bg-gray-100 px-1'>
+    <div className='w-[90%] bg-white px-1'>
 
        <StaffPicks blogs={blogs} />
-       <h3 className='text-xl my-5'> Recommend Lists </h3>
+       {/* <h3 className='text-xl my-5'> Recommend Lists </h3> */}
+       <ReadingListTip />
     </div>
   )
 }
