a8f40e36bb0fbbbfd1c450c79504d7bb76962684
Anurag <anuragmarketing101@gmail.com>
2022-11-10 12:23:05 +0530

    StaffPicksBlogCard-conditon-changed

--- frontend-nextjs/components/Sidebar/HomePageSidebar/StaffPicksBlogCard.tsx
+++ frontend-nextjs/components/Sidebar/HomePageSidebar/StaffPicksBlogCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 const StaffPicksBlogCard = ( {blog, key}:Props ) => {
   return (
     <Link href={`blog/${blog._id}`}>
-        <div key={key} className='w-full flex flex-col items-start justify-start my-2 px-1 hover:cursor-pointer'>
+        <div  className='w-full flex flex-col items-start justify-start my-2 px-1 hover:cursor-pointer'>
             <div className='w-full flex justify-start items-center space-x-2'>
                 <img src={blog.profileImg as string} alt="dp"
                     className='w-5 h-5 rounded-full'
