83b3fcb555f25afb6c2e28191f697ae4d9ba9330
Anurag <anuragmarketing101@gmail.com>
2022-11-10 12:19:52 +0530

    staffPicksBlogCard-key-add

--- frontend-nextjs/components/Sidebar/HomePageSidebar/StaffPicksBlogCard.tsx
+++ frontend-nextjs/components/Sidebar/HomePageSidebar/StaffPicksBlogCard.tsx
@@ -4,12 +4,13 @@ import Link from "next/link"
 
 interface Props {
     blog : Blog
+    key: string
 }
 
-const StaffPicksBlogCard = ( {blog}:Props ) => {
+const StaffPicksBlogCard = ( {blog, key}:Props ) => {
   return (
     <Link href={`blog/${blog._id}`}>
-        <div className='w-full flex flex-col items-start justify-start my-2 px-1 hover:cursor-pointer'>
+        <div key={key} className='w-full flex flex-col items-start justify-start my-2 px-1 hover:cursor-pointer'>
             <div className='w-full flex justify-start items-center space-x-2'>
                 <img src={blog.profileImg as string} alt="dp"
                     className='w-5 h-5 rounded-full'
