c14e20290e082bd0486957df9800237a914a6b93
Anurag <anuragmarketing101@gmail.com>
2022-11-10 13:38:28 +0530

    StaffPicksBlogCard-username-fixed

--- frontend-nextjs/components/Sidebar/HomePageSidebar/StaffPicksBlogCard.tsx
+++ frontend-nextjs/components/Sidebar/HomePageSidebar/StaffPicksBlogCard.tsx
@@ -14,7 +14,7 @@ const StaffPicksBlogCard = ( { blog }:Props ) => {
                 <img src={blog.profileImg as string} alt="dp"
                     className='w-5 h-5 rounded-full'
                     />
-                <p className='text-xs font-medium text-gray-700'> {blog.username} </p>
+                <p className='text-xs font-medium text-gray-700'> {blog.username as string} </p>
             </div>
 
             <h3 className='text-base font-bold my-1'> {blog.title} </h3>
